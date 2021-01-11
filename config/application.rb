61a0a92629e6c538c3d6ccf5f077a2a08461a033
shun-tateyama <b1522783@planet.kanazawa-it.ac.jp>
2021-01-11 14:39:58 +0900

    ADD: RubocopとRspecを導入

--- config/application.rb
+++ config/application.rb
@@ -20,3 +20,10 @@ module AppName
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+config.generators do |g|
+  g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        controller_specs: false,
+        routing_specs: false
+end
