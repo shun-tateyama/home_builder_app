a820b08fe54e7bca01863cc44d83e4638c5b3be4
shun-tateyama <b1522783@planet.kanazawa-it.ac.jp>
2021-03-23 21:07:16 +0900

    ADD: Bootstrapを導入2

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/pages/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/pages/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-end
+# RSpec.describe "Pages", type: :request do
+#   describe "GET request" do
+#     it "リクエストが成功すること" do
+#       get pages_index_path
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+#
+#   describe "GET request" do
+#     it "リクエストが成功すること" do
+#       get pages_index_path
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+# end
