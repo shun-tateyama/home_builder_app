6567b8e0ace42efa9d057163990cec3c24d30ae5
shun-tateyama <b1522783@planet.kanazawa-it.ac.jp>
2021-03-27 23:25:21 +0900

    FIX: Toppageを編集2

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
                omniauth_callbacks: 'users/omniauth_callbacks',
              }
   root to: 'pages#index'
+  get 'pages/index' => 'pages#index'
   resources :pages, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
