2405f624cdb7ca32460050e9584bf557234b3ae7
shun-tateyama <b1522783@planet.kanazawa-it.ac.jp>
2021-04-07 14:12:52 +0900

    ADD: LoginページとSignupページを追加

--- app/models/devise.rb
+++ app/models/devise.rb
@@ -1,2 +1,3 @@
 class Devise < ApplicationRecord
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i(facebook google_oauth2)
+         :omniauthable
 
   def self.without_sns_data(auth)
     user = User.where(email: auth.info.email).first
